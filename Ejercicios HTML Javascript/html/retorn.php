874c043540d213614e584f8c3854de06869a5d2a
netsergi <netsergi@gmail.com>
2017-04-20 10:21:16 +0200

    Exerciçis Actualitzats 20/04

---
+++ Ejercicios HTML Javascript/html/retorn.php
@@ -0,0 +1,11 @@
+<?php 
+	if ($_SERVER["REQUEST_METHOD"] =="POST")
+	{
+		$num1 = $_POST['num1'];
+		$num2 = $_POST['num2'];
+		echo $num1+$num2;
+	}
+	else {
+			echo "Aquest programa nomes funciona amb POST";
+	}
+ ?>
